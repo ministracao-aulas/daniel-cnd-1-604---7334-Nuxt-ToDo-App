2c572b2077365475cde30a00b7dce24d75397600
Tiago França <github@tiagofranca.com>
2025-08-12 21:09:23 -0300

    refactor: enhance todo item management with input handling and dynamic rendering

--- pages/index.vue
+++ pages/index.vue
@@ -5,95 +5,79 @@
         <div class="w-full flex flex-col gap-3 px-2 pt-4">
             <div class="w-full">
                 <UButtonGroup class="w-full">
-                    <UInput type="text" size="xl" placeholder="Add your new TODO" />
-                    <!-- <UButton type="button" class="px-3">+</UButton> -->
+                    <UInput
+                        v-model="form.content"
+                        type="text"
+                        size="xl" placeholder="Add your new TODO"
+                        @keydown.enter.prevent="addItem"
+                    />
                     <UButton
-                        size="xs"
+                        @click.prevent.stop="addItem"
+                        icon="i-lucide-plus" size="md" color="success" variant="solid"
                         type="button"
-                        class="px-3 flex justify-center content-center text-3xl font-bold text-center"
-                    >
-                        +
-                    </UButton>
+                        class="px-3 cursor-pointer flex justify-center content-center text-3xl font-bold text-center"
+                    />
                 </UButtonGroup>
             </div>
 
+            <!-- To Do -->
             <div class="w-full flex flex-col gap-3 pt-3 mb-8">
-                <div class="bg-gray-600 hover:bg-gray-600/40 h-12 w-full rounded-md grid grid-cols-5 gap-2">
-                    <div class="w-full col-span-4 ps-2 pe-3 overflow-hidden text-nowrap flex content-center">
-                        <p class="text-gray-200 text-xl pt-2">Algo aqui muito londgjhgdfhdghd ghdfgdhfgfdhdfhg</p>
+                <template v-for="(todoItem, itemIndex) in doneFalseOnly" key="itemIndex">
+                    <div class="bg-gray-600 hover:bg-gray-600/40 h-12 w-full rounded-md grid grid-cols-5 gap-2">
+                        <div class="w-full col-span-4 ps-2 pe-3 overflow-hidden text-nowrap flex content-center">
+                            <p class="text-gray-200 text-xl pt-2">{{ todoItem.content }}</p>
+                        </div>
+
+                        <div class="w-full flex gap-2 justify-between">
+                            <UButton
+                                @click.prevent.stop="deleteItem(todoItem)"
+                                icon="i-lucide-trash" size="md" color="error" variant="solid"
+                                type="button"
+                                class="px-3 cursor-pointer flex justify-center content-center text-3xl font-bold text-center"
+                            />
+
+                            <UButton
+                                @click.prevent.stop="markAsDone(todoItem)"
+                                icon="i-lucide-check" size="md" color="success" variant="solid"
+                                type="button"
+                                class="px-3 cursor-pointer flex justify-center content-center text-3xl font-bold text-center"
+                            />
+                        </div>
                     </div>
-                    <UButton
-                        type="button"
-                        class="cursor-pointer flex justify-center content-center text-3xl font-bold text-center"
-                    >
-                        +
-                    </UButton>
-                </div>
-                <div class="bg-gray-600 hover:bg-gray-600/40 h-12 w-full rounded-md grid grid-cols-5 gap-2">
-                    <div class="w-full col-span-4 ps-2 pe-3 overflow-hidden text-nowrap flex content-center">
-                        <p class="text-gray-200 text-xl pt-2">Algo aqui muito londgjhgdfhdghd ghdfgdhfgfdhdfhg</p>
-                    </div>
-                    <UButton
-                        type="button"
-                        class="cursor-pointer flex justify-center content-center text-3xl font-bold text-center"
-                    >
-                        +
-                    </UButton>
-                </div>
-                <div class="bg-gray-600 hover:bg-gray-600/40 h-12 w-full rounded-md grid grid-cols-5 gap-2">
-                    <div class="w-full col-span-4 ps-2 pe-3 overflow-hidden text-nowrap flex content-center">
-                        <p class="text-gray-200 text-xl pt-2">Algo aqui muito londgjhgdfhdghd ghdfgdhfgfdhdfhg</p>
-                    </div>
-                    <UButton
-                        type="button"
-                        class="cursor-pointer flex justify-center content-center text-3xl font-bold text-center"
-                    >
-                        +
-                    </UButton>
-                </div>
+                </template>
             </div>
 
+            <!-- Done -->
             <div class="w-full flex flex-col gap-3 pt-3 mb-8 opacity-20 hover:opacity-80">
-                <div class="bg-gray-600 hover:bg-gray-600/40 h-12 w-full rounded-md grid grid-cols-5 gap-2">
-                    <div class="w-full col-span-4 ps-2 pe-3 overflow-hidden text-nowrap flex content-center">
-                        <p class="text-gray-200 text-xl pt-2 italic line-through">
-                            Algo aqui muito londgjhgdfhdghd ghdfgdhfgfdhdfhg
-                        </p>
+                <template v-for="(todoItem, itemIndex) in doneTrueOnly" key="itemIndex">
+                    <div class="bg-gray-600 hover:bg-gray-600/40 h-12 w-full rounded-md grid grid-cols-5 gap-2">
+                        <div class="w-full col-span-4 ps-2 pe-3 overflow-hidden text-nowrap flex content-center">
+                            <p class="text-gray-200 text-xl pt-2 italic line-through">
+                                {{ todoItem.content }}
+                            </p>
+                        </div>
+                        <div class="w-full flex gap-2 justify-between">
+                            <UButton
+                                @click.prevent.stop="markAsUndone(todoItem)"
+                                icon="i-lucide-arrow-up" size="md" color="info" variant="solid"
+                                type="button"
+                                class="px-3 cursor-pointer flex justify-center content-center text-3xl font-bold text-center"
+                            />
+
+                            <UButton
+                                @click.prevent.stop="deleteItem(todoItem)"
+                                icon="i-lucide-trash" size="md" color="error" variant="solid"
+                                type="button"
+                                class="px-3 cursor-pointer flex justify-center content-center text-3xl font-bold text-center"
+                            />
+                        </div>
                     </div>
-                    <UButton
-                        type="button"
-                        class="cursor-pointer flex justify-center content-center text-3xl font-bold text-center"
-                    >
-                        +
-                    </UButton>
-                </div>
-                <div class="bg-gray-600 hover:bg-gray-600/40 h-12 w-full rounded-md grid grid-cols-5 gap-2">
-                    <div class="w-full col-span-4 ps-2 pe-3 overflow-hidden text-nowrap flex content-center">
-                        <p class="text-gray-200 text-xl pt-2">Algo aqui muito londgjhgdfhdghd ghdfgdhfgfdhdfhg</p>
-                    </div>
-                    <UButton
-                        type="button"
-                        class="cursor-pointer flex justify-center content-center text-3xl font-bold text-center"
-                    >
-                        +
-                    </UButton>
-                </div>
-                <div class="bg-gray-600 hover:bg-gray-600/40 h-12 w-full rounded-md grid grid-cols-5 gap-2">
-                    <div class="w-full col-span-4 ps-2 pe-3 overflow-hidden text-nowrap flex content-center">
-                        <p class="text-gray-200 text-xl pt-2">Algo aqui muito londgjhgdfhdghd ghdfgdhfgfdhdfhg</p>
-                    </div>
-                    <UButton
-                        type="button"
-                        class="cursor-pointer flex justify-center content-center text-3xl font-bold text-center"
-                    >
-                        +
-                    </UButton>
-                </div>
+                </template>
             </div>
 
             <div class="w-full flex flex-col gap-3 pt-3 mb-8">
-                <div class="div flex frex-row gap-3 justify-between content-center">
-                    <h4 class="text-2xl text-gray-200 font-bold">You have X pending tasks</h4>
+                <div v-show="doneFalseOnly.length" class="div flex frex-row gap-3 justify-between content-center">
+                    <h4 class="text-2xl text-gray-200 font-bold">You have {{ doneFalseOnly.length || 0 }} pending tasks</h4>
                     <UButton
                         type="button"
                         size="xs"
@@ -108,5 +92,114 @@
 </template>
 
 <script setup lang="ts">
-//
+
+type TodoItem = {
+    id: string,
+    content: string,
+    done?: boolean|null,
+}
+
+const todoList = ref<TodoItem[]>([
+    {
+        id: 'abc1',
+        content: 'Item 1',
+        done: true,
+    },
+    {
+        id: 'abc2',
+        content: 'Item 2',
+        done: false,
+    },
+    {
+        id: 'abc3',
+        content: 'Item 3',
+        done: false,
+    },
+    {
+        id: 'abc4',
+        content: 'Item 4',
+        done: false,
+    },
+    {
+        id: 'abc5',
+        content: 'Item 5',
+        done: false,
+    },
+    {
+        id: 'abc6',
+        content: 'Item 6',
+        done: true,
+    },
+    {
+        id: 'abc7',
+        content: 'Item 7',
+        done: false,
+    },
+    {
+        id: 'abc8',
+        content: 'Item 8',
+        done: false,
+    },
+    {
+        id: 'abc9',
+        content: 'Item 9',
+        done: true,
+    },
+    {
+        id: 'abc10',
+        content: 'Item 10',
+        done: false,
+    },
+    {
+        id: 'abc11',
+        content: 'Item 11',
+        done: true,
+    },
+    {
+        id: 'abc12',
+        content: 'Item 12',
+        done: true,
+    },
+]);
+
+const form = reactive({
+    done: false,
+    content: '',
+})
+
+const doneFalseOnly = computed(() => todoList.value.filter((i: TodoItem) => !i.done));
+const doneTrueOnly = computed(() => todoList.value.filter((i: TodoItem) => i.done));
+
+const markAsDone = (item: TodoItem) => {
+    item.done = true;
+}
+
+const markAsUndone = (item: TodoItem) => {
+    item.done = false;
+}
+
+const deleteItem = (item: TodoItem) => {
+    todoList.value = todoList.value.filter((i: TodoItem) => i.id !== item.id);
+}
+
+const resetForm = () => {
+    form.done = false;
+    form.content = '';
+}
+
+const generateId = () => Math.random.toString().slice(2, 5);
+const addItem = () => {
+    if (typeof form.content !== 'string' || !form.content.trim().length) {
+        return;
+    }
+
+    let item: TodoItem = {
+        id: generateId(),
+        done: Boolean(form.done || false),
+        content: form.content.trim(),
+    };
+
+    todoList.value.unshift(item);
+    resetForm();
+}
 </script>
